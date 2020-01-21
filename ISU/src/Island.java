a6737d5a9e85b60b64630fd0c0c8af828f5500c5
rebec <rebec@192.168.0.14>
2020-01-21 14:28:55 -0500

    game

---
+++ ISU/src/Island.java
@@ -0,0 +1,20 @@
+
+import org.lwjgl.util.Rectangle;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+
+abstract public class Island { 
+    
+    private Rectangle hitbox;
+    private Image island;
+    
+    
+    public Island (int x, int y) throws SlickException {
+     
+        
+                
+    }
+    
+    
+}
